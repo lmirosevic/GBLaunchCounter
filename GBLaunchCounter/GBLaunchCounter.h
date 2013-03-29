491d5b3e61178b822fe14fb9d83236d4fcad4d5c
Luka Mirosevic <luka@goonbee.com>
2013-03-29 12:40:28 +0100

    Initial Commit

---
+++ GBLaunchCounter/GBLaunchCounter.h
@@ -0,0 +1,13 @@
+//
+//  GBLaunchCounter.h
+//  GBLaunchCounter
+//
+//  Created by Luka Mirosevic on 29/03/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBLaunchCounter : NSObject
+
+@end
---
+++ GBLaunchCounter/GBLaunchCounter.m
@@ -0,0 +1,13 @@
+//
+//  GBLaunchCounter.m
+//  GBLaunchCounter
+//
+//  Created by Luka Mirosevic on 29/03/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "GBLaunchCounter.h"
+
+@implementation GBLaunchCounter
+
+@end
